84a8be1afd7063f2b4ad7abec052e641885e3231
Uhyune <annone@hanmaul.net>
2021-03-08 15:11:28 +0900

    added ouput-BitKeeper

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
 	public static void main(String[] args{
-		System.out.print("Hello");
-		System.out.print("What is git");
-}}
+		System.out.println("Hello");
+		System.out.println("What is git");
+		System.out.println("linux kernel used Bitkeeper DVCS");
+
+	}
+
+}
